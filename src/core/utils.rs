461eaa61a13180cf54398ae208c4be48236e8ee6
“Frigyes06” <eszfrigyes06@gmail.com>
2023-12-27 15:21:25 -0800

    Changed default theme

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -56,7 +56,7 @@ pub fn string_to_theme(theme: &str) -> Theme {
         "Dark" => Theme::Dark,
         "Light" => Theme::Light,
         "Lupin" => Theme::Lupin,
-        _ => Theme::Lupin,
+        _ => Theme::Dark,
     }
 }
 
