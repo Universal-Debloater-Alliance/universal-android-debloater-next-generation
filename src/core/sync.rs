Universal-Debloater-Alliance/universal-android-debloater-next-generation

commit 6ffba9d40efad82c8129904ef38385c9ae30f288

refactor: select optional user based on sdk, then build request
use the boolean .then_some method to create an `Option`al user based on the Android SDK, then build a request

src/core/sync.rs
@@ -240,11 +240,8 @@ pub fn apply_pkg_state_commands(
         },
         PackageState::All => vec![],
     };
-    if phone.android_sdk < 21 {
-        request_builder(&commands, &package.name, None)
-    } else {
-        request_builder(&commands, &package.name, Some(selected_user))
-    }
+    let user = (phone.android_sdk < 21).then_some(selected_user);
+    request_builder(&commands, &package.name, user)
 }
 
 pub fn request_builder(commands: &[&str], package: &str, user: Option<&User>) -> Vec<String> {

commit 37a96980cebb4cf6fc7bb1301b231ba7cc8a286c

fix: pass user flag when sdk version is 21 or above
fixes regression introduced in 6ffba9d40efad82c8129904ef38385c9ae30f288

src/core/sync.rs
@@ -240,7 +240,7 @@ pub fn apply_pkg_state_commands(
         },
         PackageState::All => vec![],
     };
-    let user = (phone.android_sdk < 21).then_some(selected_user);
+    let user = (phone.android_sdk >= 21).then_some(selected_user);
     request_builder(&commands, &package.name, user)
 }
 