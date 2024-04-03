8bdeb40de0b7b02554c88b66582c47b34bd70ae5
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-04-03 09:00:23 +0530

    feat: update flake.nix for wayland compatibility

--- flake.nix
+++ flake.nix
@@ -1,37 +1,45 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
-  };
+  description = "devshell for uad-ng";
 
-  outputs = {
-    systems,
-    nixpkgs,
-    ...
-  }: let
-    eachSystem = f:
-      nixpkgs.lib.genAttrs (import systems) (
-        system:
-          f nixpkgs.legacyPackages.${system}
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell rec {
+            packages = with pkgs;
+            [
+              rustc
+              cargo
+              makeWrapper
+              clang
+              pkg-config
+              mold
+              expat
+              fontconfig
+              freetype
+              android-tools
+              libxkbcommon
+              libGL
+              wayland
+            ];
+
+            LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath [
+              pkgs.fontconfig
+              pkgs.freetype
+              pkgs.libglvnd
+              pkgs.xorg.libX11
+              pkgs.xorg.libXcursor
+              pkgs.xorg.libXi
+              pkgs.xorg.libXrandr
+              pkgs.libxkbcommon
+              pkgs.libGL
+              pkgs.wayland
+            ]}";
+            LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
+          };
+        }
       );
-  in {
-    devShells = eachSystem (pkgs: {
-      default = pkgs.mkShell rec {
-        buildInputs = with pkgs; [
-          clang
-          mold
-          libGL
-          # WINIT_UNIX_BACKEND=wayland
-          wayland
-          android-tools
-          libxkbcommon
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
-          xorg.libX11
-        ];
-        LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath buildInputs}";
-      };
-    });
-  };
 }
