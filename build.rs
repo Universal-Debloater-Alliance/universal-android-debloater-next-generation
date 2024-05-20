9cc1799f3083c02e4b53df717dd92a7074a615db
nozwock <57829219+nozwock@users.noreply.github.com>
2024-05-20 20:13:16 +0530

    Fix file path typo in `build.rs`

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::{env, error::Error};
 
 fn main() -> Result<(), Box<dyn Error>> {
     if env::var("CARGO_CFG_TARGET_FAMILY")? == "windows" {
-        embed_resource::compile("resources/assets/manifest.rs", embed_resource::NONE);
+        embed_resource::compile("resources/assets/manifest.rc", embed_resource::NONE);
     }
 
     Ok(())
