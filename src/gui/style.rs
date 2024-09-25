146a15943c65a40aa68e5e51d210b4b97fd01e7d
Frigyes Erdosi Szucs <eszfrigyes06@gmail.com>
2024-09-25 12:23:31 -0700

    build(test): added some more unit tests

--- src/gui/style.rs
+++ src/gui/style.rs
@@ -573,3 +573,23 @@ impl rule::StyleSheet for Theme {
         }
     }
 }
+
+// Unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_palette() {
+        let palette = Theme::default().palette();
+
+        assert_ne!(palette.base.background, Color::BLACK);
+        assert_ne!(palette.base.foreground, Color::BLACK);
+        assert_ne!(palette.normal.primary, Color::BLACK);
+        assert_ne!(palette.normal.surface, Color::BLACK);
+        assert_ne!(palette.bright.primary, Color::BLACK);
+        assert_ne!(palette.bright.surface, Color::BLACK);
+        assert_ne!(palette.normal.error, Color::BLACK);
+        assert_ne!(palette.bright.error, Color::BLACK);
+    }
+}
