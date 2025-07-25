83b5542e3f0a5aca891ad4e9a1d61883f9d27013
Rudxain <76864299+Rudxain@users.noreply.github.com>
2025-07-24 23:11:42 -0400

    build: only rerun `build.rs` if relevant files change

--- build.rs
+++ build.rs
@@ -1,9 +1,10 @@
-use std::{env, error::Error};
+const META: &str = "resources/assets/manifest.rc";
 
-fn main() -> Result<(), Box<dyn Error>> {
-    if env::var("CARGO_CFG_TARGET_FAMILY")? == "windows" {
-        embed_resource::compile("resources/assets/manifest.rc", embed_resource::NONE);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed={META}");
+    if std::env::var("CARGO_CFG_TARGET_FAMILY")? == "windows" {
+        embed_resource::compile(META, embed_resource::NONE);
     }
-
     Ok(())
 }
