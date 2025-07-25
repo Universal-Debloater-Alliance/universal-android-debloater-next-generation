3aa7f56d0cc6c915e98aaf6f763d75a38d591ead
Rudxain <76864299+Rudxain@users.noreply.github.com>
2025-07-24 23:16:04 -0400

    build: `unwrap` `embed_resource::compile`

--- build.rs
+++ build.rs
@@ -3,7 +3,9 @@ const META: &str = "resources/assets/manifest.rc";
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo::rerun-if-changed={META}");
     if std::env::var("CARGO_CFG_TARGET_FAMILY")? == "windows" {
-        embed_resource::compile(META, embed_resource::NONE);
+        embed_resource::compile(META, embed_resource::NONE)
+            .manifest_optional()
+            .unwrap();
     }
     Ok(())
 }
