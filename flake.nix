ef3803eba5c2a9677dec0cc1e447f9c2dd784c10
iamanaws <78835633+Iamanaws@users.noreply.github.com>
2025-08-23 15:40:37 -0700

    chore: update flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -1,36 +1,56 @@
 {
   description = "devshell for uad-ng";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      forSystems =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: f { pkgs = import nixpkgs { inherit system; }; }
+        );
+    in
+    {
+      formatter = forSystems ({ pkgs }: pkgs.nixfmt-tree);
+
+      devShells = forSystems (
+        { pkgs }:
         {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs;
-            [
+          default = pkgs.mkShell {
+            packages = with pkgs; [
               rustc
               cargo
               clang
+
+              rustfmt
+              rust-analyzer
+              clippy
+
               pkg-config
               android-tools
             ];
 
-            LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath [
-              pkgs.fontconfig
-              pkgs.freetype
-              pkgs.libglvnd
-              pkgs.xorg.libX11
-              pkgs.xorg.libXcursor
-              pkgs.xorg.libXi
-              pkgs.xorg.libXrandr
-              pkgs.libxkbcommon
-              pkgs.wayland
-            ]}";
-            LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}/lib/rustlib/src/rust";
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (
+              with pkgs;
+              [
+                fontconfig
+                freetype
+                libglvnd
+                xorg.libX11
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXrandr
+                libxkbcommon
+                wayland
+              ]
+            )}";
           };
         }
       );
+    };
 }
